ae90dada8fcd0959906f13fc100d7575de0eed25
LorenzoLemmi <lorenzo.lemmi92@gmail.com>
2025-01-07 18:19:55 +0100

    feat: Add function number even or odd

--- JS/main.js
+++ JS/main.js
@@ -22,4 +22,30 @@ function randomNumber (min, max) {
 }
 const selectedRandomNumber = randomNumber (1, 5);
 
+// Funzione per determinare se un numero è pari o dispari
+function sumOfNumbers (num1, num2) {
+    return (num1 + num2) % 2 == 0;
+}
 
+
+let inputEvenOrOdd = prompt("Inserisci <Pari> o <Dispari>").toLocaleLowerCase;
+let inputNumber;
+
+if (inputEvenOrOdd !== "pari" || inputEvenOrOdd !== "dispari" || inputEvenOrOdd !== "Pari" || inputEvenOrOdd !== "Dispari") {
+    inputEvenOrOdd = prompt("Inserisci <Pari> o <Dispari>");
+} else {
+    inputNumber = prompt("Inserisci un numero da 1 a 5");
+    if (inputNumber > 5 || inputNumber <1) {
+        inputNumber = prompt("Inserisci un numero da 1 a 5");
+    } 
+}
+
+
+
+
+
+if (((sumOfNumbers (inputNumber, selectedRandomNumber) === true) && (inputEvenOrOdd == "Pari" || inputEvenOrOdd == "pari")) || ((sumOfNumbers (inputNumber, selectedRandomNumber) === false) && (inputEvenOrOdd == "Dispari" || inputEvenOrOdd == "dispari"))) { 
+    alert("Hai vinto!") 
+    } else { 
+    alert("Hai perso!") 
+}
